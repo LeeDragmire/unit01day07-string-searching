3d423c9edd7b4b6e5dfc9015c1bcd275f65ee258
colton <adamscolton64@gmail.com>
2023-01-25 11:38:42 -0600

    String Searching | FindTagContents Complete

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class FindTagContents {
     public static void main(String[] args) {
+        System.out.println("Enter HTMl Text here: ");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println("Now enter tag to search for: ");
+        String search = scanner.nextLine();
+        int textPos = text.indexOf(search);
+        int closePos = text.indexOf("</", textPos);
+        int endClosePos = text.indexOf(">", closePos);
+        if (text.substring(closePos + 2, endClosePos).equals(search)) {
+            int startPos = text.indexOf(">", textPos);
+            int endPos = text.indexOf("<", textPos + 1);
+            System.out.println(text.substring(startPos + 1, endPos));
+        } else {
+            System.out.println("there are no '" + search + "' tags in the entered HTML");
+        }
         
+
+        scanner.close();
     }
 }
